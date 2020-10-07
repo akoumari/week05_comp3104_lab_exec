8bbe5f921243d13d40dbbf0cf8faec7cb186ef69
akoumari <alex.koumarianos@gmail.com>
2020-10-07 19:50:17 -0400

    added all new files

---
+++ build/precache-manifest.2caa0c39d689e4faddbce36612e732d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3405d399de9009c73c14",
+    "url": "/static/css/main.0da1ee79.chunk.css"
+  },
+  {
+    "revision": "3405d399de9009c73c14",
+    "url": "/static/js/main.3405d399.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "e3d173c23999b0362175a579f01eb90a",
+    "url": "/index.html"
+  }
+];
