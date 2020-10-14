cfd1bb1b572ee67f89184f66906e5c710f3a8d2c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-14 23:31:12 +0000

    Deploy akoumari/week05_comp3104_lab_exec to github.com/akoumari/week05_comp3104_lab_exec.git:gh-pages

---
+++ precache-manifest.d3bea6651e95ed3648bbdc5a5d7ec1d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "404d9b1a05829254cfd9",
+    "url": "/static/css/main.2bea53d5.chunk.css"
+  },
+  {
+    "revision": "404d9b1a05829254cfd9",
+    "url": "/static/js/main.404d9b1a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "54b023bbd83be237114d2217ec87e7db",
+    "url": "/index.html"
+  }
+];
